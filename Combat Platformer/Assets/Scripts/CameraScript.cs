7c267f3869341ab87e71b01e1b247efb2354c810
manelsj <manelsjacob@gmail.com>
2025-02-17 15:54:51 -0500

    Camera Adjustment
    Adjusted The camera so that it only tracks the player once it gets too far.

--- Combat Platformer/Assets/Scripts/CameraScript.cs
+++ Combat Platformer/Assets/Scripts/CameraScript.cs
@@ -6,11 +6,31 @@ public class CameraScript : MonoBehaviour
 {
     public GameObject playerGO;
 
-    public Vector3 offset;
+    public float zOffset;
+
+    public float horizontalOffset;
+
+    private Vector3 position;
+
+    void Start()
+    {
+        position = transform.position;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position = playerGO.transform.position + offset;
+        if (playerGO.transform.position.x > position.x + horizontalOffset)
+        {
+            position.x = playerGO.transform.position.x - horizontalOffset;
+        }
+        else if (playerGO.transform.position.x < position.x - horizontalOffset)
+        {
+            position.x = playerGO.transform.position.x + horizontalOffset;
+        }
+
+
+        position = new Vector3(position.x, playerGO.transform.position.y, playerGO.transform.position.z + zOffset);
+        transform.position = position;
     }
 }
