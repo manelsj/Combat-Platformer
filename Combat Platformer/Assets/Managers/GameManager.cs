1de4ed6ae762802bb9cf071a5396e2711848b77d
manelsj <manelsjacob@gmail.com>
2025-06-13 15:37:56 -0400

    Game and Level Manager
    Added game and level managers and gave basic functionality.

--- Combat Platformer/Assets/Managers/GameManager.cs
+++ Combat Platformer/Assets/Managers/GameManager.cs
@@ -1,19 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> levelList = new List<GameObject>();
+    public List<string> levelNameList = new List<string>();
+    private int levelNum;
     // Start is called before the first frame update
     void Start()
     {
-        
+        levelNum = 0;
+        loadLevel(levelNum);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void loadLevel(int sceneNum)
+    {
+        SceneManager.LoadScene(levelNameList[sceneNum]);
+    }
+
+    public void nextLevel()
+    {
+        levelNum++;
+        loadLevel(levelNum);
     }
 }
--- Combat Platformer/Assets/Managers/LevelManager.cs
+++ Combat Platformer/Assets/Managers/LevelManager.cs
@@ -7,16 +7,33 @@ public class LevelManager : MonoBehaviour
     public int levelNum;
     public List<GameObject> checkpoints = new List<GameObject>();
     private int currentCheck;
-    public int lastCheck;
+    private int lastCheck;
+    public GameObject gameManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        lastCheck = checkpoints.Count;
+        currentCheck = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void nextCheckpoint()
+    {
+        currentCheck++;
+
+        if (currentCheck == checkpoints.Count) {
+            nextLevel();
+        }
+
+    }
+
+    private void nextLevel()
+    {
+        gameManager.GetComponent<GameManager>().nextLevel();
     }
 }
